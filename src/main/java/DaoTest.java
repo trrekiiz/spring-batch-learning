b0e46e855f01edec674b18dba1197494d05e6b4e
Kunchai <trrekiiz@camphub.in.th>
2018-02-19 02:10:40 +0700

    init project

---
+++ src/main/java/DaoTest.java
@@ -0,0 +1,8 @@
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
+
+/**
+ * Created by wluo on 3/8/2017.
+ */
+public class DaoTest extends NamedParameterJdbcDaoSupport {
+
+}
---
+++ src/main/java/batch/Message.java
@@ -0,0 +1,7 @@
+package batch;
+
+public class Message {
+    private String content;
+    public String getContent() {return content;}
+    public void setContent(String content) {this.content = content;}
+}
